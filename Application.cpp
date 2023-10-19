63fe6bbcafccddcc913ced7f02ff28386c12077e
kushwanthchavidi <123448054+kushwanthchavidi@users.noreply.github.com>
2023-10-19 10:42:38 +0530

    Add files via upload

---
+++ Application.cpp
@@ -0,0 +1,26 @@
+//                                                                           n                          nb nlm#include "Application.h"
+
+// Constructors/Destructors
+//  
+
+Application::Application () {
+initAttributes();
+}
+
+Application::~Application () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void Application::initAttributes () {
+}
+
